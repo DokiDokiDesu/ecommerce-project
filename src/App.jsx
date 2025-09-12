915ba889fdca09c11d424288109d2fcc163355f0
DokiDokiDesu <demre421423@hotmail.com>
2025-09-12 16:43:27 +0300

    header component updated. Links established

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { Orders } from "./pages/Orders";
+import { Tracking } from "./pages/Tracking";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<Orders />} />
+      <Route path="tracking" element={<Tracking />} />
+    </Routes>
+  );
+}
+
+export default App;
